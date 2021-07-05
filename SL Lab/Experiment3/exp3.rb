652a5063b4b1871db6b34203a9bc59b78462feb5
18wh1a1234 <69299896+18wh1a1234@users.noreply.github.com>
2021-07-05 09:58:42 +0530

    Create exp3.rb

---
+++ SL Lab/Experiment3/exp3.rb
@@ -0,0 +1,6 @@
+puts "Enter first name: "
+str1 = gets.chomp
+puts "Enter second name: "
+str2 = gets.chomp
+puts "The result is #{str2} #{str1}"
+puts "The result is #{str2.reverse} #{str1.reverse}"
