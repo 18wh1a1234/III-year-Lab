42f9c324ad1a7f80c9fa1bc4fce7ee1bd8607a1a
18wh1a1234 <69299896+18wh1a1234@users.noreply.github.com>
2021-07-05 09:56:54 +0530

    Create exp2.rb

---
+++ SL Lab/Experiment2/exp2.rb
@@ -0,0 +1,8 @@
+puts "Enter the radius value: "
+radius = gets.chomp.to_f
+perimeter = 0.0
+area = 0.0
+perimeter = 2 * 3.14 * radius
+area = 3.14 * radius * radius
+puts "The perimeter of a circle is #{perimeter}"
+puts "The area of a circle is #{area}"
