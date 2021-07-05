6a31e9523e41beb352e3c0670c32432118749efb
18wh1a1234 <69299896+18wh1a1234@users.noreply.github.com>
2021-07-05 09:48:39 +0530

    Create exp1_2Method.rb

---
+++ SL Lab/Experiment1/exp1_2Method.rb
@@ -0,0 +1,10 @@
+#Second method
+def stringcopy(str,n)
+    return str*n
+end
+puts "The result is:"
+puts stringcopy('x',1)
+puts stringcopy('x',2)
+puts stringcopy('x',3)
+puts stringcopy('x',4)
+puts stringcopy('x',5)
